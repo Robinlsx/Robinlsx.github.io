969912770a7abba3f67e2c5a9d25cfdd2d2da0f2
Nsfz202317 <Robinlee19@yandex.com>
2024-06-16 15:21:47 +0800

    v1.0-240616

--- Chemistry/ddr.js
+++ Chemistry/ddr.js
@@ -1,5 +1,5 @@
-var yin=["OH","NO<sub>3<sub>","Cl","SO<sub>4<sub>","CO<sub>3<sub>"];
-var yang=["H","NH<sub>4<sub>","K","Na","Ba","Ca","Mg","Al","Mn","Zn","Fe","Fe","Cu","Ag"];
+var yin=["OH","NO<sub>3</sub>","Cl","SO<sub>4</sub>","CO<sub>3</sub>"];
+var yang=["H","NH<sub>4</sub>","K","Na","Ba","Ca","Mg","Al","Mn","Zn","Fe","Fe","Cu","Ag"];
 var yinn=["氢氧化","硝酸","氯化","硫酸","碳酸"];
 var yangn=["氢","铵","钾","钠","钡","钙","镁","铝","锰","锌","亚铁","铁","铜","银"];
 var yinv=[-1,-1,-1,-2,-2];
@@ -21,25 +21,60 @@ var rjx=[
     [-1,0,3,1,2]
 ];
 
-var Ayang=Math.floor(Math.random()*15);
-var Ayin=Math.floor(Math.random()*5);
-var Byang=Math.floor(Math.random()*15);
-var Byin=Math.floor(Math.random()*5);
-
-while(Ayang==0 && Ayin==4) Ayang=Math.floor(Math.random()*15),Ayin=Math.floor(Math.random()*5);
-while(Byang==0 && Byin==4) Byang=Math.floor(Math.random()*15),Byin=Math.floor(Math.random()*5);
-
-function compound(yang,yin)
+function compound(yangno,yinno)
 {
+    if(yangno==0 && yinno==0) return "H<sub>2</sub>O";
+    if(yangno==1 && yinno==0) return "NH<sub>3</sub>·H<sub>2</sub>O";
     var yangstl="",yangstr="",yinstl="",yinstr="";
-    if(yang==1) yangstl="(",yangstr=")";
-    if(yin!=2) yinstl="(",yinstr=")";
-    if(yinv[yin]==-1 && yangv[yang]==1) return yangn[yang]+yinn[yin];
-    if(yinv[yin]==-1 && yangv[yang]==2) return yangn[yang]+yinstl+yinn[yin]+yinstr+"<sub>2</sub>";
-    if(yinv[yin]==-1 && yangv[yang]==3) return yangn[yang]+yinstl+yinn[yin]+yinstr+"<sub>3</sub>";
-    if(yinv[yin]==-2 && yangv[yang]==1) return yangstl+yangn[yang]+yangstr+"<sub>2</sub"+yinn[yin];
-    if(yinv[yin]==-2 && yangv[yang]==2) return yangn[yang]+yinn[yin];
-    if(yinv[yin]==-2 && yangv[yang]==3) return yangstl+yangn[yang]+yangstr+"<sub>2</sub>"+yinstl+yinn[yin]+yinstr+"<sub>3</sub>";
+    if(yangno==1) yangstl="(",yangstr=")";
+    if(yinno!=2) yinstl="(",yinstr=")";
+    if(yinv[yinno]==-1 && yangv[yangno]==1) return yang[yangno]+yin[yinno];
+    if(yinv[yinno]==-1 && yangv[yangno]==2) return yang[yangno]+yinstl+yin[yinno]+yinstr+"<sub>2</sub>";
+    if(yinv[yinno]==-1 && yangv[yangno]==3) return yang[yangno]+yinstl+yin[yinno]+yinstr+"<sub>3</sub>";
+    if(yinv[yinno]==-2 && yangv[yangno]==1) return yangstl+yang[yangno]+yangstr+"<sub>2</sub>"+yin[yinno];
+    if(yinv[yinno]==-2 && yangv[yangno]==2) return yang[yangno]+yin[yinno];
+    if(yinv[yinno]==-2 && yangv[yangno]==3) return yangstl+yang[yangno]+yangstr+"<sub>2</sub>"+yinstl+yin[yinno]+yinstr+"<sub>3</sub>";
 }
 
-document.getElementById("prob").innerHTML=compound(7,3);
+function getCompoundName(yangno,yinno)
+{
+    if(yangno==0 && yinno==0) return "水";
+    if(yangno==1 && yinno==0) return "氨水";
+    if(yangno==0 && (yinno==1 || yinno==3 || yinno==4)) return yinn[yinno];
+    return yinn[yinno]+yangn[yangno];
+}
+
+function test()
+{
+    for(var i=0; i<5; i++)
+        for(var j=0; j<14; j++)
+            document.getElementById("prob").innerHTML+="<br>"+compound(j,i)+" "+getCompoundName(j,i);
+}
+
+function generateProblem()
+{
+    var Ayang=Math.floor(Math.random()*14);
+    var Ayin=Math.floor(Math.random()*5);
+    var Byang=Math.floor(Math.random()*14);
+    var Byin=Math.floor(Math.random()*5);
+
+    //while (Ayang == 15) Ayang = Math.floor(Math.random() * 15);
+    //while (Byang == 15) Byang = Math.floor(Math.random() * 15);
+    //while (Ayin == 5) Ayin = Math.floor(Math.random() * 5);
+    //while (Byin == 5) Byin = Math.floor(Math.random() * 5);
+    
+    while(Ayang==0 && Ayin==4) Ayang=Math.floor(Math.random()*14),Ayin=Math.floor(Math.random()*5);
+    while(Byang==0 && Byin==4) Byang=Math.floor(Math.random()*14),Byin=Math.floor(Math.random()*5);
+    while(Byang==Ayang && Byin==Ayin) Byang=Math.floor(Math.random()*14),Byin=Math.floor(Math.random()*5);
+
+    console.log("A=["+Ayang+","+Ayin+"], B=["+Byang+","+Byin+"]")
+    document.getElementById("prob").innerHTML+="写出"+getCompoundName(Ayang,Ayin)+"和"+getCompoundName(Byang,Byin)+"反应的化学方程式：<br>";
+}
+
+function getProblem()
+{
+    var probNum=parseInt(document.getElementById("probNum").value);
+    document.getElementById("prob").innerHTML="";
+    for(var _=0; _<probNum; _++)
+        document.getElementById("prob").innerHTML+=(_+1)+". ",generateProblem();
+}
